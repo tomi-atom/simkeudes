0199d1b9c4b5da460afcd864ea5ff6557eeefb2d
atomf <tomifirman@student.unri.ac.id>
2021-08-20 16:37:06 +0700

    first Commit

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
     error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 }
 
-//Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => ['web', 'cekuser:1']], function(){
 	Route::get('admin', function() {
